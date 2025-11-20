9f1d5b839db97d99e53f5008800ba79c8a91744e
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-11-20 10:01:00 -0700

    chart changes

--- frontend/public/scripts/weather_charts.js
+++ frontend/public/scripts/weather_charts.js
@@ -5,75 +5,6 @@ const orderedDays = [];
 const hourGraph = document.getElementById('forecastHourlyGraph');
 const dayGraph = document.getElementById('forecastDailyGraph');
 
-const hourlyTempArray = [];
-const dailyTemps = [];
-
-const hours = [];
-const hourlyTemps = [];
-
-const dailyTempMax = [];
-const dailyTempMin = [];
-
-const hourlyChart = new Chart({
-    type: 'line',
-    data: {
-        labels: hours,
-        datasets: [{
-            data: hourlyTemps,
-            fill: false,
-            borderColor: 'rgb(235, 196, 0)',
-            tension: .5
-        }]
-    },
-    options: {
-        plugins: {
-            legend: {
-                display: false
-            }
-        },
-        scales: {
-            x: {
-                title: {
-                    display: true,
-                    text: 'Hours from current'
-                }
-            }
-        }
-    }
-});
-
-const dailyChart = new Chart({
-    type: 'line',
-    data: {
-        labels: orderedDays,
-        datasets: [{
-            label: 'Max',
-            data: dailyTempMax,
-            fill: false,
-            borderColor: 'rgb(255, 136, 0)',
-            tension: .2
-        },
-        {
-            label: 'Min',
-            data: dailyTempMin,
-            fill: false,
-            borderColor: 'rgb(0, 93, 255)',
-            tension: .2
-        }]
-    },
-    options: {
-        plugins: {
-            legend: {
-                display: true,
-                labels: {
-                    boxWidth: 15,
-                    boxHeight: 1
-                }
-            }
-        }
-    }
-});
-
 for (let i = 0; i < 7; i++) {
     const dayIndex = (todayIndex + i) % 7;
     orderedDays.push(daysOfWeek[dayIndex]);
@@ -87,15 +18,76 @@ document.addEventListener("weatherDataReady", (event) => {
 
 // render the weather charts
 function renderForecastGraph(data) {
-    hourlyTempArray = data?.hourly.slice(0, 12);
-    dailyTemps = data?.daily.slice(0, 7);
+    const hourlyTempArray = data?.hourly.slice(0, 12);
+    const dailyTemps = data?.daily.slice(0, 7);
 
-    hours = Array.from({length: 12}, (_, index) => index + 1);
-    hourlyTemps = hourlyTempArray.map(temps => temps.temp);
+    const hours = Array.from({length: 12}, (_, index) => index + 1);
+    const hourlyTemps = hourlyTempArray.map(temps => temps.temp);
 
-    dailyTempMax = dailyTemps.map(dailyTemp => dailyTemp.temp.max);
-    dailyTempMin = dailyTemps.map(dailyTemps => dailyTemps.temp.min);
+    const dailyTempMax = dailyTemps.map(dailyTemp => dailyTemp.temp.max);
+    const dailyTempMin = dailyTemps.map(dailyTemps => dailyTemps.temp.min);
 
-    hourlyChart.update();
-    dailyChart.update();
+    new Chart(
+        document.getElementById('forecastHourlyGraph'),
+        {
+            type: 'line',
+            data: {
+                labels: hours,
+                datasets: [{
+                    data: hourlyTemps,
+                    fill: false,
+                    borderColor: 'rgb(235, 196, 0)',
+                    tension: .5
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Hours from current'
+                        }
+                    }
+                }
+            }
+        });
+
+        new Chart(
+        document.getElementById('forecastDailyGraph'),
+            {
+            type: 'line',
+            data: {
+                labels: orderedDays,
+                datasets: [{
+                    label: 'Max',
+                    data: dailyTempMax,
+                    fill: false,
+                    borderColor: 'rgb(255, 136, 0)',
+                    tension: .2
+                },
+                {
+                    label: 'Min',
+                    data: dailyTempMin,
+                    fill: false,
+                    borderColor: 'rgb(0, 93, 255)',
+                    tension: .2
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: true,
+                        labels: {
+                            boxWidth: 15,
+                            boxHeight: 1
+                        }
+                    }
+                }
+            }
+        });
 }
